33d6a2ccf6b0b22a38a81f2c7c8763a8bf08aacb
flow-ryan <florian.scherer@rematiq.com>
2024-04-16 23:25:47 +0200

    Update config.py

--- req_analysis/config.py
+++ req_analysis/config.py
@@ -7,6 +7,6 @@ GPT4_TURBO = "GPT-4-TURBO"
 GPT35_TURBO = "GPT-35T"
 
 OPENAI_API_TYPE = "azure"
-OPENAI_API_BASE = "https://clr-openai-france.openai.azure.com/"
+OPENAI_API_BASE = "https://clr-openai-hiring.openai.azure.com/"
 OPENAI_API_VERSION = "2023-05-15"
 OPENAI_API_KEY = os.getenv("AZ_OPENAI_API_KEY")
